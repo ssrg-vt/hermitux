164692ff9d114127a9c2dd2139ccbd757f215b49
Pierre Olivier <polivier@vt.edu>
2017-11-20 11:46:24 -0500

    Small fixes

--- apps/osv/ep.a/npbparams.h
+++ apps/osv/ep.a/npbparams.h
@@ -1,14 +1,14 @@
-/* CLASS = B */
+/* CLASS = A */
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
    in this directory. Do not modify it by hand.   
 */
-#define CLASS  'B'
-#define M      30
+#define CLASS  'A'
+#define M      28
 
 #define CONVERTDOUBLE  false
-#define COMPILETIME "17 Nov 2017"
+#define COMPILETIME "20 Nov 2017"
 #define NPBVERSION "3.3.1"
 #define CS1 "(none)"
 #define CS2 "(none)"
