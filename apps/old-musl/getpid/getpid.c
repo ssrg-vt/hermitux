2b0f7440d7519ed486b4889f3648f8d4761fdc90
Pierre Olivier <polivier@vt.edu>
2018-04-25 15:58:43 -0400

    Cleanup

--- apps/old-musl/getpid/getpid.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(void) {
-	printf("PID: %d\n", getpid());
-	return 0;
-}
--- apps/old-musl/ioctl-modular/syscall-disabler.py
+++
@@ -1,1 +0,0 @@
-../../../tools/syscall-disabler/syscall-disabler.py
