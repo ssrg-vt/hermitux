df57bdfb15822bce4f362ac5c8dade3ed1c8ac66
Pierre Olivier <polivier@vt.edu>
2018-10-09 18:55:21 -0400

    Updates some network tests

--- apps/redis-2.0.4/hermitux.sh
+++ apps/redis-2.0.4/hermitux.sh
@@ -8,7 +8,9 @@ PROG=./redis-server
 #ARGS='--port 8000 --protected-mode no --save "" --appendonly no'
 ARGS='redis.conf'
 MINIFS=0
+PROF=0
 
 HERMIT_MEM=2G HERMIT_VERBOSE=$VERBOSE HERMIT_ISLE=uhyve HERMIT_TUX=1 \
 	HERMIT_MINIFS=$MINIFS HERMIT_MINIFS_HOSTLOAD=.minifs HERMIT_NETIF=tap100 \
-	$PROXY $KERNEL $PROG $ARGS
+	HERMIT_PROFILE=$PROF HERMIT_CPUS=1 \
+	numactl --physcpubind=1 $PROXY $KERNEL $PROG $ARGS
