e1b2af28b8df06491e81a38359cece0f71468cea
Pierre Olivier <polivier@vt.edu>
2018-10-23 19:51:41 -0400

    TLS tests

--- apps/loader-glibc/hello.c
+++ apps/loader-glibc/hello.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <sys/auxv.h>
 
+__thread int x = 13;
+
 int main(int argc, char **argv) {
-	
+
 	printf("argv[0] = %s\n", argv[0]);
 	printf("AT_BASE: %#lx\n", getauxval(AT_BASE));
 	printf("AT_RANDOM: %#lx\n", getauxval(AT_RANDOM));
+
+	printf("TLS var: %d\n", x);
 
 	return 0;
 }
