72884c32f33ebaea00a58f6f6589d5b0e1a839e2
Pierre Olivier <polivier@vt.edu>
2018-12-04 10:50:31 -0500

    extend so test

--- apps/loader-dyn/hello.c
+++ apps/loader-dyn/hello.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
-	
+	double a;
+
 	printf("argv[0] = %s\n", argv[0]);
+	a = sqrt(4);
+
+	printf("%f\n", a);
+
+	printf("pid: %d\n", getpid());
 
 	return 0;
 }
