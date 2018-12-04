e9bfe648c3134be7641ee040617854bb7e30af0c
Pierre Olivier <polivier@vt.edu>
2018-12-04 16:47:25 -0500

    Update dyn test

--- apps/loader-dyn/hello.c
+++ apps/loader-dyn/hello.c
@@ -5,7 +5,8 @@
 int main(int argc, char **argv) {
 	double a;
 
-	printf("argv[0] = %s\n", argv[0]);
+	for(int i=0; i<argc; i++)
+		printf("argv[%d] = %s\n", i, argv[i]);
 	a = sqrt(4);
 
 	printf("%f\n", a);
