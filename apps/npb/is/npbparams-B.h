404dd94002894ace6479ade998e31f058e529685
Pierre Olivier <polivier@vt.edu>
2019-09-11 19:05:14 -0400

    Small updates for NPB benchs

---
+++ apps/npb/is/npbparams-B.h
@@ -0,0 +1,14 @@
+#define CLASS 'B'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "17 Nov 2017"
+#define NPBVERSION "3.3.1"
+#define CC "(none)"
+#define CFLAGS "(none)"
+#define CLINK "(none)"
+#define CLINKFLAGS "(none)"
+#define C_LIB "(none)"
+#define C_INC "(none)"
