6a215c0be5b511b08fec09e2e6a96877afdde3a5
Pierre Olivier <polivier@vt.edu>
2019-09-29 17:34:12 -0400

    Add multiple classes for npb CG

---
+++ apps/npb/cg/npbparams-A.h
@@ -0,0 +1,22 @@
+/* CLASS = A */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define NA      14000
+#define NONZER  11
+#define NITER   15
+#define SHIFT   20.0
+#define RCOND   1.0e-1
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "28 Sep 2019"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=medium"
+#define CS6 "-O3 -mcmodel=medium"
+#define CS7 "randdp"
---
+++ apps/npb/cg/npbparams-S.h
@@ -0,0 +1,22 @@
+/* CLASS = S */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define NA      1400
+#define NONZER  7
+#define NITER   15
+#define SHIFT   10.0
+#define RCOND   1.0e-1
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "28 Sep 2019"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=medium"
+#define CS6 "-O3 -mcmodel=medium"
+#define CS7 "randdp"
