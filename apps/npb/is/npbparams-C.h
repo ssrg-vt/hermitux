4eabed1ae5a14aa4b9d70e5e9022fc47aa386998
Pierre Olivier <polivier@vt.edu>
2019-09-30 16:09:08 -0400

    More classes for some of the NPB benchmarks

---
+++ apps/npb/is/npbparams-C.h
@@ -0,0 +1,14 @@
+#define CLASS 'C'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "30 Sep 2019"
+#define NPBVERSION "3.3.1"
+#define CC "gcc"
+#define CFLAGS "-g -Wall -O3 -mcmodel=medium"
+#define CLINK "$(CC)"
+#define CLINKFLAGS "-O3 -mcmodel=medium"
+#define C_LIB "-lm"
+#define C_INC "-I../common"
