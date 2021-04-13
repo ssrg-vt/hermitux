47d86da5e5e96743016003a1ed8268ba16580c39
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2021-04-13 13:22:04 +0100

    reorganize apps

--- apps/osv/README-other-apps.txt
+++
@@ -1,2 +0,0 @@
-Look here:
-https://github.com/ssrg-vt/osv/blob/pierre-hermitux/README.pierre.md
--- apps/osv/mg.b/print_results.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRINT_RESULTS_H__
-#define __PRINT_RESULTS_H__
-
-void print_results(char *name, char class, int n1, int n2, int n3, int niter,
-    double t, double mops, char *optype, logical verified, char *npbversion,
-    char *compiletime, char *cs1, char *cs2, char *cs3, char *cs4, char *cs5,
-    char *cs6, char *cs7);
-
-#endif //__PRINT_RESULTS_H__
