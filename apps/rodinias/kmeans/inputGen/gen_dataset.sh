b8e5031b93ecbb85638d354a93fd1e64f0d4f948
p-jacquot <pierre.jacquot@insa-lyon.fr>
2021-05-07 11:23:57 +0200

    Removed too big input file.

---
+++ apps/rodinias/kmeans/inputGen/gen_dataset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./datagen 100
+./datagen 300
+./datagen 1000
+./datagen 3000
+./datagen 10000
+./datagen 30000
+./datagen 100000
+./datagen 300000
+./datagen 1000000
+./datagen 3000000
+./datagen 10000000
+./datagen 100 -f
+./datagen 300 -f
+./datagen 1000 -f
+./datagen 3000 -f
+./datagen 10000 -f
+./datagen 30000 -f
+./datagen 100000 -f
+./datagen 300000 -f
+./datagen 1000000 -f
+./datagen 3000000 -f
+./datagen 10000000 -f
+
