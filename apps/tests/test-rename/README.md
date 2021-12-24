4296045d2e5a09b607d914801216ae67ec334ad9
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2021-12-24 15:55:35 +0100

    add rename test

---
+++ apps/tests/test-rename/README.md
@@ -0,0 +1,19 @@
+# Simple test exercising the `rename()` system call
+
+To compile, edit if needed in the `Makefile` the `HERMITUX_BASE` variable to
+reflect the location of HermiTux base folder on your computer. Then, simply
+type:
+
+```
+make
+```
+
+To run the test under Linux:
+```
+./prog
+```
+
+To run the test under HermiTux:
+```
+make test
+```
---
+++ apps/tests/test-rename/rename.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <syscall.h>
+
+#define SRC_FILE "src-file.dat"
+#define DST_FILE "dst-file.dat"
+#define FILE_SZ  4096
+
+int main(void) {
+    int fd=0;
+
+    /* delete any existing file */
+    remove(SRC_FILE);
+    remove(DST_FILE);
+
+    /* create the source file */
+    fd = open(SRC_FILE, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+    if(fd == -1) {
+        perror("open");
+        exit(-1);
+    }
+
+    for(int i=0; i<FILE_SZ; i++)
+        if(write(fd, "@", 1) != 1) {
+            perror("write");
+            exit(-1);
+        }
+
+    close(fd);
+
+    /* rename */
+    if(rename(SRC_FILE, DST_FILE)) {
+        perror("rename");
+        exit(-1);
+    }
+
+    /* check destination file */
+    fd = open(DST_FILE, O_RDONLY, NULL);
+    if(fd == -1) {
+        perror("open");
+        exit(-1);
+    }
+
+    for(int i=0; i<FILE_SZ; i++) {
+        char c;
+        if(read(fd, &c, 1) != 1) {
+            perror("wrread");
+            exit(-1);
+        }
+
+        if(c != '@') {
+            fprintf(stderr, "destination file content check failed\n");
+            exit(-1);
+        }
+    }
+
+    close(fd);
+
+	return 0;
+}
